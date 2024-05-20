ffa6d7284ac430ee628da575c43e48850d285186
Neeraj Sharma <nesharm78@gmail.com>
2024-05-20 02:24:53 -0700

    added check for not loaded prefs

--- final/src/common/menu.service.js
+++ final/src/common/menu.service.js
@@ -8,7 +8,7 @@ angular.module('common')
 MenuService.$inject = ['$http', 'ApiPath'];
 function MenuService($http, ApiPath) {
   var service = this;
-  var preferences = {};
+  var preferences = null;
 
   service.getCategories = function () {
     return $http.get(ApiPath + '/categories.json').then(function (response) {
--- final/src/public/myinfo/myinfo.controller.js
+++ final/src/public/myinfo/myinfo.controller.js
@@ -5,6 +5,13 @@
     .controller('MyinfoController', ['$scope', '$http', 'MenuService', function($scope, $http, MenuService){
         
         $scope.myinfoPrefs = MenuService.getSignupPrefs();
+        console.log($scope.myinfoPrefs);
+        if ($scope.myinfoPrefs == null) {
+            $scope.showprefs = false;
+        } else {
+            $scope.showprefs = true;
+        }
+        
         //{"firstName": "ffff", "lastName": "sdsd", "email": "sdf@dfdf.com", "phone": "2343244", "short_name": "L2"};  
     }]);
     
